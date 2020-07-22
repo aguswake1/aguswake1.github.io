13c80ad14e2c5ca9ecba59046d78a909bd0ce8bc
Agustin Olano <aguswake4@gmail.com>
2020-07-22 15:45:34 -0300

    Subo 2 proyectos

---
+++ Proyecto Taste/assets/scripts/script.js
@@ -0,0 +1,26 @@
+// Cambio animación cuando se cierra cada blog.
+let regresar = document.querySelectorAll(".volver");
+
+regresar.forEach(function(volver){
+    volver.addEventListener("click", function(ev){
+        ev.preventDefault();
+        let content = document.querySelector(".content");
+
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
+
+        content.classList.add("animate__fadeOutUp");
+        content.classList.add("animate__animated");
+
+        // Antes de volver al home doy tiempo para que se ejecute la animación.
+        setTimeout(function (){
+            location.href = "/";
+        },350)
+        
+        
+
+        return false;
+    });
+});
+
+
